dd33f35f56fdd67bc77c2d4f31b00b61b9c134f5
Vaibhav Singh <vaibhavsingh2712@gmail.com>
2022-07-14 09:06:19 +0530

    Importing Libs and setting image parameters

--- imageCaptioning.py
+++ imageCaptioning.py
@@ -1,2 +1,17 @@
 # Implementation of an image captioning model using Convolutional Neural Networks
 
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.applications import efficientnet
+from tensorflow.keras.layers import TextVectorization
+
+
+seed = 111
+np.random.seed(seed)
+tf.random.set_seed(seed)
