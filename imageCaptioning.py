6a749da7c7b40a2b49d74f49007b80c0203ba6e6
Vaibhav Singh <vaibhavsingh2712@gmail.com>
2022-07-14 09:10:13 +0530

    Setting up the caption data

--- imageCaptioning.py
+++ imageCaptioning.py
@@ -15,3 +15,79 @@ from tensorflow.keras.layers import TextVectorization
 seed = 111
 np.random.seed(seed)
 tf.random.set_seed(seed)
+
+
+# Path to the images
+IMAGES_PATH = "Flicker8k_Dataset"
+
+# Desired image dimensions
+IMAGE_SIZE = (299, 299)
+
+# Vocabulary size
+VOCAB_SIZE = 10000
+
+# Fixed length allowed for any sequence
+SEQ_LENGTH = 25
+
+# Dimension for the image embeddings and token embeddings
+EMBED_DIM = 512
+
+# Per-layer units in the feed-forward network
+FF_DIM = 512
+
+# Other training parameters
+BATCH_SIZE = 64
+EPOCHS = 30
+AUTOTUNE = tf.data.AUTOTUNE
+
+
+
+## DATASET PREPARATION
+
+def load_captions_data(filename):
+    """Loads captions (text) data and maps them to corresponding images.
+    Args:
+        filename: Path to the text file containing caption data.
+    Returns:
+        caption_mapping: Dictionary mapping image names and the corresponding captions
+        text_data: List containing all the available captions
+    """
+
+    with open(filename) as caption_file:
+        caption_data = caption_file.readlines()
+        caption_mapping = {}
+        text_data = []
+        images_to_skip = set()
+
+        for line in caption_data:
+            line = line.rstrip("\n")
+            # Image name and captions are separated using a tab
+            img_name, caption = line.split("\t")
+
+            # Each image is repeated five times for the five different captions.
+            # Each image name has a suffix `#(caption_number)`
+            img_name = img_name.split("#")[0]
+            img_name = os.path.join(IMAGES_PATH, img_name.strip())
+
+            # We will remove caption that are either too short to too long
+            tokens = caption.strip().split()
+
+            if len(tokens) < 5 or len(tokens) > SEQ_LENGTH:
+                images_to_skip.add(img_name)
+                continue
+
+            if img_name.endswith("jpg") and img_name not in images_to_skip:
+                # We will add a start and an end token to each caption
+                caption = "<start> " + caption.strip() + " <end>"
+                text_data.append(caption)
+
+                if img_name in caption_mapping:
+                    caption_mapping[img_name].append(caption)
+                else:
+                    caption_mapping[img_name] = [caption]
+
+        for img_name in images_to_skip:
+            if img_name in caption_mapping:
+                del caption_mapping[img_name]
+
+        return caption_mapping, text_data
